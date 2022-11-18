ac58cf13e16c2793e3f2746f115868ffa8394dd7
Anton Dulya <108669385+Duuuuuuulya@users.noreply.github.com>
2022-11-18 11:34:14 +0300

    second eiler

--- Eiler.js
+++ Eiler.js
@@ -14,6 +14,29 @@ const one = () =>{
   return summ
 }
 
+const two = () => {
+  // const array = [0, 1];
+  // const summ = 0;
+  // const counter  = array.reduce((previousValue, currentValue) => previousValue + currentValue)
+  // while (counter !== 4000000) {
+  //   array.push(counter)
+  //   if(counter % 2 === 0) {
+  //     summ = summ + counter
+  // }
+  // console.log(summ)
+  // return summ
+  // }
+  let counter = 0
+  let x = 1
+  let y = 1
+  let c = x+y
+  while (c < 4000000){
+  counter += c
+  x = y + c
+  y = c + x
+  c = x + y
+  }
+  console.log(counter)
+}
 
-
-AnswerBtn.addEventListener('click', one);
+AnswerBtn.addEventListener('click', two);
